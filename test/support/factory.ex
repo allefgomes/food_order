42710960d1155a8879f890219b17c3e1c9514ca1
Allef Gomes <allefgalmeida@gmail.com>
2022-04-19 20:26:58 -0300

    add exmachina and faker libs

---
+++ test/support/factory.ex
@@ -0,0 +1,12 @@
+defmodule FoodOrder.Factory do
+  use ExMachina.Ecto, repo: FoodOrder.Repo
+
+  def product_factory do
+    %FoodOrder.Products.Schemas.Product{
+      description: Faker.Lorem.paragraph(),
+      name: Faker.Food.dish(),
+      price: [100, 200, 300] |> Enum.random,
+      size: ["small", "big", "normal"] |> Enum.random
+    }
+  end
+end
