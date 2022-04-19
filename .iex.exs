ec32ba6a9ded0106fa12b098e10fa0e8a1c38236
Allef Gomes <allefgalmeida@gmail.com>
2022-04-19 20:25:59 -0300

    create file to read when run mix

---
+++ .iex.exs
@@ -0,0 +1,7 @@
+# aliases of phoenix
+alias FoodOrder.Repo
+alias FoodOrderWeb.Router.Helpers, as: Routes
+
+# aliases
+alias FoodOrder.Products.Schemas.Product
+alias FoodOrder.Products.Repositories.ProductsRepository
